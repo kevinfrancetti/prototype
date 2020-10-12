7d8e8dcfb5b1584b38e59fc4c05b59f7422b9b1b
Scrotuss <scroto@protonmail.ch>
2020-10-12 22:35:41 +0200

    1

---
+++ Makefile
@@ -0,0 +1,13 @@
+.SUFFIXES: .c .o
+
+say_hello:
+	@echo "Mucca dio cane"
+	
+build:
+	gcc -v -o aids main.c
+
+build-debug:
+	gcc -g -o aids-debug main.c
+	
+clean:
+	rm -v aids.exe
---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: 12 ott 2020
+ *      Author: Roxanji
+ */
+
+#include <stdio.h>
+
+int main(){
+	printf("Dio boia");
+	printf("mannaggia");
+	int a = 4;
+	int b = 12;
+	return 0;
+}
+
