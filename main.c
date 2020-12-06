3e3cf6af632b4ed956f5bee5e026d1d39674542d
kevinfrancetti <kevin.francetti91@gmail.com>
2020-12-06 11:27:24 +0100

    antorher escape seq2

--- main.c
+++ main.c
@@ -9,6 +9,21 @@
 #include "prog1.h"
 #include "snake.h"
 
+int try(void)
+{
+  int i, j, n;
+
+  for (i = 0; i < 11; i++) {
+    for (j = 0; j < 10; j++) {
+      n = 10*i + j;
+      if (n > 108) break;
+      printf("\033[%dm %3d\033[m", n, n);
+    }
+    printf("\n");
+  }
+  return (0);
+}
+
 int main(){
 	//hello();
 	int a = 4;
